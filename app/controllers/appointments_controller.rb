50c57aeff2408152e6ac70c66fa3a4d8d639176e
David P. Hanesana <dhanesana@gmail.com>
2015-07-09 03:32:20 -0700

    Display 5 upcoming appointments in table

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,14 @@
 class AppointmentsController < ApplicationController
+
   def index
+    today_pdt = Time.now.utc + Time.zone_offset('PDT')
+    @appointments = []
+    Appointment.all.each do |appointment|
+      if appointment.class_date > today_pdt
+        @appointments << appointment unless appointment.users.size > 29
+        break if @appointments.size > 4
+      end
+    end
   end
 
   def new
@@ -7,4 +16,5 @@ class AppointmentsController < ApplicationController
 
   def create
   end
+
 end
