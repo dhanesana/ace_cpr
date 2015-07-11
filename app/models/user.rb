50568c3799872a4c86fd419b48dcf87ce78084e0
David P. Hanesana <dhanesana@gmail.com>
2015-07-11 15:53:47 -0700

    Add validation for email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, presence: true
+  validates :email, uniqueness: true
 end
