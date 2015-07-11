8b568a0fb5a65fc41955ac168b3e2df1586b65f1
David P. Hanesana <dhanesana@gmail.com>
2015-07-11 02:54:28 -0700

    Fix display of minute

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ActiveRecord::Base
   has_many :users
 
   def formated_date
-    class_date.strftime("%B %d, %Y %I:%m %p")
+    class_date.strftime("%B %d, %Y %I:%M %p")
   end
 
 end
