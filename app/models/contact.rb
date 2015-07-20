0fe1fbba0d759386daa58ed60003983c56a76632
David P. Hanesana <dhanesana@gmail.com>
2015-07-20 04:08:12 -0700

    Delete comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,2 @@
 class Contact < ActiveRecord::Base
-# require 'open-uri'
-# require 'geocoder'
-
-#   def latitude
-#     response = Geocoder.search("#{street} #{area}")
-#     return response.first.data['geometry']['location']['lat']
-#   end
-
-#   def longitude
-#     15
-#   end
 end
