493060df664fff3acc89868e0eedee6f275d67ad
David P. Hanesana <dhanesana@gmail.com>
2015-07-20 05:13:10 -0700

    Update contact information

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -9,7 +9,7 @@ $(document).on('page:change', function() {
     map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
     marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(32.832334, -117.14482900000002)});
     infowindow = new google.maps.InfoWindow({
-      content:"<a href='https://maps.google.com?q=8333+Clairmont+Mesa+Blvd+#21+San+Diego+92111'><b>Ace CPR San Diego</b></a><br/>8333 Clairemont Mesa Blvd #21<br/>San Diego, 92111" });
+      content:"<a href='https://maps.google.com?q=8333+Clairmont+Mesa+Blvd+#215+San+Diego+92111'><b>Ace CPR San Diego</b></a><br/>8333 Clairemont Mesa Blvd #215<br/>San Diego, 92111" });
     google.maps.event.addListener(marker, "click", function() {
       infowindow.open(map,marker);
     });
