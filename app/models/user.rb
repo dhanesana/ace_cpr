06571ffdb68ab2fa0856380ee83d322a75e10392
David P. Hanesana <dhanesana@gmail.com>
2015-08-15 00:00:32 -0700

    Remove turbolinks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :appointment_id, presence: true
   validates :phone, presence: true
-  validates :email, uniqueness: true
+  validates :email, presence: true
 
   phony_normalize :phone, as: :formatted_phone, default_country_code: 'US'
 end
