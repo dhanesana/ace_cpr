ec5ebcf2e14d8b3f94e46d58a270ec8ca25544b8
David P. Hanesana <dhanesana@gmail.com>
2015-08-15 00:26:18 -0700

    Permit params for activeadmin resource "type"

--- app/admin/type.rb
+++ app/admin/type.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Type do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :list, :of, :attributes, :on, :model, :name, :blurb, :description, :cost, :image_url
 #
 # or
 #
