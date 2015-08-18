57e746f667e19518472a16bdd194e4b45bccb90b
David P. Hanesana <dhanesana@gmail.com>
2015-08-18 12:01:16 -0700

    Fix: Only <td>'s with type class are links

--- app/assets/javascripts/tr_link.js
+++ app/assets/javascripts/tr_link.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $('td').click(function() {
+  $('.types').click(function() {
     window.location = $(this).find('a').attr('href');
   }).hover(function() {
     $(this).toggleClass('hover');
