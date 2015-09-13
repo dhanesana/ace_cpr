a79060f8930dce13934411dff660f4ef24b904d8
David P. Hanesana <dhanesana@gmail.com>
2015-09-12 23:28:21 -0700

    Refactor, add comments, and don't call @coupon.limit if @coupon is nil

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -21,30 +21,20 @@ class CouponsController < ApplicationController
     end
     @appointments = @appointments.sort_by { |x| x.class_date }
     @user = User.new
-    @coupon = Coupon.where(code: input, type_id: params[:type_id]).first
-    @coupon = nil if @coupon.limit < 1
     @price = Type.where(id: params[:type_id]).first.cost
+    @coupon = Coupon.where(code: input, type_id: params[:type_id]).first
+    # coupon can't be redeemed if its limit has reached 0
     if @coupon
-      @type = Type.where(id: params[:type_id]).first
+      @coupon = nil if @coupon.limit < 1
+    end
+    if @coupon
       @price = @price - @coupon.discount.to_i
       session[:price] = @price
       @redeemed = 1
       session[:coupon] = @coupon
       render :template => 'types/show'
     else
-      @type = Type.where(id: params[:type_id]).first
-      @appointments = []
-      @type.appointments.each do |appointment|
-        @appointments << appointment if appointment.users.size > 0 && appointment.class_date.utc > Time.now.utc && appointment.users.size < 9
-        if (appointment.class_date.utc - 43200) > Time.now.utc
-          next if @appointments.include? appointment
-          @appointments << appointment unless appointment.users.size > 7
-        end
-      end
-      @appointments = @appointments.sort_by { |x| x.class_date }
-      @user = User.new
       @coupon = Coupon.new
-      @price = Type.where(id: params[:type_id]).first.cost
       @redeemed = 0
       @invalid = 1
       render :template => 'types/show'
