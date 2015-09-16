318c9cba3aa04f63956def9d303d053701e5b187
David P. Hanesana <dhanesana@gmail.com>
2015-09-15 17:34:10 -0700

    Add handling of failed charges and display failed charge message

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -22,6 +22,7 @@ class TypesController < ApplicationController
     @coupon = Coupon.new
     @redeemed = 0
     @user_error = 0
+    # @card_error = 0
     @refund_policy = Refund.first
     session[:price] = nil
     session[:coupon] = nil
@@ -37,6 +38,7 @@ class TypesController < ApplicationController
     else
       @price = @type.cost
     end
+    @amount = @price * 100
     @user = User.new(
       first_name: params[:user][:first_name],
       last_name: params[:user][:last_name],
@@ -44,26 +46,24 @@ class TypesController < ApplicationController
       email: params[:stripeEmail],
       appointment_id: params[:user][:appointment_id]
     )
+    # send payment information to Stripe
+    customer = Stripe::Customer.create(
+      :description => "Class Date: #{@appointment.class_date.strftime("%B %d, %Y %I:%M %p")}",
+      :email => params[:stripeEmail],
+      :card  => params[:stripeToken]
+    )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => "Class Date: #{@appointment.class_date.strftime("%B %d, %Y %I:%M %p")}",
+      :currency    => 'usd'
+    )
+
     if @user.save
       if session[:coupon]
         @coupon.limit -= 1
         @coupon.save
       end
-      @amount = @price * 100
-
-      # send payment information to Stripe
-      customer = Stripe::Customer.create(
-        :description => "Class Date: #{@appointment.class_date.strftime("%B %d, %Y %I:%M %p")}",
-        :email => params[:stripeEmail],
-        :card  => params[:stripeToken]
-      )
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => "Class Date: #{@appointment.class_date.strftime("%B %d, %Y %I:%M %p")}",
-        :currency    => 'usd'
-      )
-
       # send confirmation email (template: send_signup_email )
       UserNotifier.send_signup_email(@user).deliver_now
     else
@@ -89,7 +89,27 @@ class TypesController < ApplicationController
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to charges_path
+    @price = @type.cost
+    @types = Type.all
+    @appointments = []
+    @type.appointments.each do |appointment|
+      @appointments << appointment if appointment.users.size > 0 && appointment.class_date.utc > Time.now.utc && appointment.users.size < 9
+      # 43200 is 12 hours (43200 seconds)
+      if (appointment.class_date.utc - 43200) > Time.now.utc
+        next if @appointments.include? appointment
+        @appointments << appointment unless appointment.users.size > 7
+      end
+    end
+    @appointments = @appointments.sort_by { |x| x.class_date }
+    @user = User.new
+    @coupon = Coupon.new
+    @redeemed = 0
+    @user_error = 0
+    @refund_policy = Refund.first
+    session[:price] = nil
+    session[:coupon] = nil
+    @card_error = 1
+    render :template => 'types/show'
   end
 
 
