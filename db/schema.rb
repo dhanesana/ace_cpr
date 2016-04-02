4adf720473d4dafe4a84cba49d8d9372860e8728
David P. Hanesana <dhanesana@gmail.com>
2016-04-01 21:06:34 -0700

    Add image_url to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402035918) do
+ActiveRecord::Schema.define(version: 20160402040542) do
 
   create_table "about_bullets", force: :cascade do |t|
     t.text     "bullet"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20160402035918) do
     t.integer  "type_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image_url"
   end
 
   create_table "combos", force: :cascade do |t|
