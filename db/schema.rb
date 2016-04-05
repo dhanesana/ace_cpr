9bd1c8f1b6c34c605d81dbb4a2901f25eb7d1af3
David P. Hanesana <dhanesana@gmail.com>
2016-04-05 02:40:50 -0700

    Add purchased_book column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402040542) do
+ActiveRecord::Schema.define(version: 20160405093740) do
 
   create_table "about_bullets", force: :cascade do |t|
     t.text     "bullet"
@@ -169,8 +169,9 @@ ActiveRecord::Schema.define(version: 20160402040542) do
     t.string   "email"
     t.string   "phone"
     t.string   "formatted_phone"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "purchased_book",  default: false
   end
 
   add_index "users", ["appointment_id"], name: "index_users_on_appointment_id"
